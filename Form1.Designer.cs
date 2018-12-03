47c83bd2204607db42e6df89ec0711d9d642b37d
Mcneb10 <mcneb10@yahoo.com>
2018-12-03 06:13:20 -0600

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Eva_Compiler_And_IDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.highlightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Output = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 206);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.highlightToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compileToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // compileToolStripMenuItem
+            // 
+            this.compileToolStripMenuItem.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Compile;
+            this.compileToolStripMenuItem.Name = "compileToolStripMenuItem";
+            this.compileToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.compileToolStripMenuItem.Text = "Compile";
+            this.compileToolStripMenuItem.Click += new System.EventHandler(this.compileToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Save;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Image = global::Eva_Compiler_And_IDE.Properties.Resources.New;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Open;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Exit;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // highlightToolStripMenuItem
+            // 
+            this.highlightToolStripMenuItem.Name = "highlightToolStripMenuItem";
+            this.highlightToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.highlightToolStripMenuItem.Text = "Highlight";
+            this.highlightToolStripMenuItem.Click += new System.EventHandler(this.highlightToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // Output
+            // 
+            this.Output.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Output.FormattingEnabled = true;
+            this.Output.Location = new System.Drawing.Point(12, 290);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(776, 160);
+            this.Output.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(376, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Compile;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Compile";
+            this.toolStripButton1.Click += new System.EventHandler(this.compileToolStripMenuItem_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Save;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Save";
+            this.toolStripButton2.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::Eva_Compiler_And_IDE.Properties.Resources.New;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "New";
+            this.toolStripButton3.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Open;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "Open";
+            this.toolStripButton4.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = global::Eva_Compiler_And_IDE.Properties.Resources.Exit;
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "Exit";
+            this.toolStripButton5.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem highlightToolStripMenuItem;
+        private System.Windows.Forms.ListBox Output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace Eva_Compiler_And_IDE
+{
+    public partial class Form1 : Form
+    {
+        bool saved = false;
+        string lastpath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            saved = false;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "EVA Files|*.eva";
+            if(s.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream str = File.Open(s.FileName, FileMode.CreateNew))
+                using(StreamWriter sw = new StreamWriter(str))
+                {
+                    sw.Write(richTextBox1.Text + "\n");
+                }
+                saved = true;
+                lastpath = s.FileName;
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            saved = false;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream s;
+            OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "EVA Files|*.eva";
+            if(o.ShowDialog() == DialogResult.OK)
+            {
+                if((s = o.OpenFile()) != null)
+                {
+                    string file = o.FileName;
+                    string contents = File.ReadAllText(file);
+                    richTextBox1.Text = contents;
+                    saved = true;
+                    lastpath = o.FileName;
+                }
+            }
+        }
+
+        private void highlightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool doresave = false;
+            if(saved == true)
+            {
+                doresave = true;
+            }
+            int index = 0;
+            String text = richTextBox1.Text;
+            richTextBox1.Text = "";
+            richTextBox1.Text = text;
+            string[] keywords = { "printe", "printdebug", "printerror", "inputprint", "mb", "new", "islegiteva", "dir", "dl", "info", "lastconsoleitem", "speak", "inputspeak", "sleep", "requires" };
+            foreach (string keyword in keywords)
+            {
+                while (index < richTextBox1.Text.LastIndexOf(keyword))
+                {
+                    richTextBox1.Find(keyword, index, richTextBox1.TextLength, RichTextBoxFinds.WholeWord);
+                    richTextBox1.SelectionColor = Color.Purple;
+                    index = richTextBox1.Text.IndexOf(keyword, index) + 1;
+
+                }
+                index = 0;
+            }
+            if(doresave)
+            {
+                saved = true;
+            }
+        }
+
+        private void compileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            if(saved)
+            {
+                SoundPlayer s = new SoundPlayer(Properties.Resources.Windows_XP_Logon_Sound);
+                s.Play();
+                compile(lastpath);
+            } else
+            {
+                
+                SoundPlayer s = new SoundPlayer(Properties.Resources.Windows_XP_Critical_Stop);
+                s.Play();
+                MessageBox.Show("You need to save the file first");
+            }
+        }
+        bool error = false;
+        public void compile(string path)
+        {
+            Output.Items.Clear();            
+             if (!File.ReadAllText(path).ToLower().Contains("islegiteva"))
+            {
+                error = true;
+                SoundPlayer s = new SoundPlayer(Properties.Resources.Windows_XP_Critical_Stop);
+                s.Play();
+                Output.Items.Add("File Not Legit. Add islegiteva to the top of your file");
+                Output.Items.Add("Error Code 1");
+            } else if (File.ReadAllText(path).IndexOf("islegiteva") > 1)
+            {
+                error = true;
+                SoundPlayer s = new SoundPlayer(Properties.Resources.Windows_XP_Critical_Stop);
+                s.Play();
+                Output.Items.Add("You cannot have more than one islegiteva keyword in a file");
+                Output.Items.Add("Error Code 2");
+            } else if(File.ReadAllText(path).Contains("new") && !File.ReadAllText(path).Contains("requires IO") || File.ReadAllText(path).Contains("dir") && !File.ReadAllText(path).Contains("requires IO") || File.ReadAllText(path).Contains("dl") && !File.ReadAllText(path).Contains("requires IO") || File.ReadAllText(path).Contains("info") && !File.ReadAllText(path).Contains("requires IO"))
+            {
+                error = true;
+                SoundPlayer s = new SoundPlayer(Properties.Resources.Windows_XP_Critical_Stop);
+                s.Play();
+                Output.Items.Add("You must includ the IO Package to use these functions add 'requires IO' to your code");
+                Output.Items.Add("Error Code 3");
+            }
+            else if (!error)
+            {
+                Thread.Sleep(1000);
+                SoundPlayer s = new SoundPlayer(Properties.Resources.tada);
+                s.Play();
+                string[] lines = File.ReadAllLines(path);
+                    foreach (string line in lines)
+                    {
+                        if (line.ToLower().StartsWith("printe"))
+                        {
+                            Output.Items.Add(line.Remove(0, 7));
+                        }
+                        if (line.ToLower().StartsWith("printdebug"))
+                        {
+                        Output.Items.Add("Debbuger: " + line.Remove(0, 11));
+                        }
+                        if (line.ToLower().StartsWith("printerror"))
+                        {
+                        Output.Items.Add("ERROR: " + line.Remove(0, 11));
+                        }
+                        if (line.ToLower().StartsWith("inputprint"))
+                        {
+                        string result;
+                        string text = line.Remove(0, 11);
+                        result = Interaction.InputBox(text, text);
+                        Output.Items.Add(result);
+                        }
+                        if(line.ToLower().StartsWith("mb"))
+                        {
+                        string g = line.Remove(0, 3);
+                        MessageBox.Show(g, g);
+                        } if (line.ToLower().StartsWith("new"))
+                        {
+                        try
+                        {
+                            File.Create(line.Remove(0, 4));
+                        } catch { }
+                        }
+                        if(line.ToLower().StartsWith("dir"))
+                        {
+                        try
+                        {
+                            Directory.CreateDirectory(line.Remove(0, 4));
+                        } catch { }
+                        }
+                        if(line.ToLower().StartsWith("dl"))
+                        {
+                        string pathd = line.Remove(0, 3);
+                            if(File.Exists(pathd)) {
+                            File.Delete(pathd);
+                            } else if(Directory.Exists(pathd))
+                            {
+                            Directory.Delete(pathd);
+                            } else
+                            {
+                            Output.Items.Add("Item " + pathd + " does not exist");
+                            }
+                        }
+                        if(line.ToLower().StartsWith("info"))
+                        {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(".");
+                        foreach (DirectoryInfo d in directoryInfo.GetDirectories())
+                        {
+                            Output.Items.Add("Current dir contains dir " + d.Name);
+                        }
+                        foreach(FileInfo f in directoryInfo.GetFiles())
+                        {
+                            Output.Items.Add("Current dir contains file " + f.Name);
+                        }
+                        }
+                    if (line.ToLower().StartsWith("sleep"))
+                    {
+                        int sleepytime;
+                        try
+                        {
+                            sleepytime = Int32.Parse(line.Remove(0, 6));
+                            Thread.Sleep(sleepytime * 1000);
+                        } catch { }
+                    }
+                    if(line.ToLower().StartsWith("lastconsoleitem"))
+                    {
+                        Output.Items.Add(Output.Items[Output.Items.Count-1]);
+                    }
+                    if(line.ToLower().StartsWith("speak"))
+                    {
+                        string stringtospeak = line.Remove(0, 6);
+                        speak(stringtospeak);
+                    }
+                    if(line.ToLower().StartsWith("inputspeak"))
+                    {
+                        string stringtospeak = line.Remove(0, 11);
+                        speak(stringtospeak);
+                        string result = Interaction.InputBox(stringtospeak, stringtospeak);
+                        Output.Items.Add(result);
+                    }
+                    if(line.ToUpper().StartsWith("sound"))
+                    {
+                        string pat;
+                        if (File.Exists(line.Remove(0, 6)))
+                        {
+                            pat = line.Remove(0, 6);
+                        }
+                        SoundPlayer soundPlayer = new SoundPlayer(pat);
+                    }
+                }
+            }
+        }
+        private static SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public static void speak(string stringtospeak)
+        {
+            if(stringtospeak != "")
+            {
+                synthesizer.Dispose();
+                synthesizer = new SpeechSynthesizer();
+                synthesizer.SpeakAsync(stringtospeak);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Properties.Resources.Credits, "Credits");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Properties.Resources.Credits;
+        }
+    }
+}
